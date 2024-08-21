8af96e56034141856d632d67c9b4c6eef0a3938a
Kiattisak-N <kiatti.env@gmail.com>
2024-08-21 15:06:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import streamlit as st
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+
+df = sns.load_dataset('iris')
+df
+
+x = df.iloc[:, :-1]
+y = df['species']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+st.sidebar.title('Classifiers')
 classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM', 'DT', 'RF', 'NN'))
 k = st.sidebar.slider('K', 1, 120, 3)
 if classifier == 'KNN':
